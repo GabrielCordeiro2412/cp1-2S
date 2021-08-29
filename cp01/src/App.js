e2a4d34d2ce60db9fbc273ff507df6997d7eb99b
VitorSouzaNzF <rm87827@fiap.com.br>
2021-08-28 23:11:29 -0300

    first commit

--- cp01/src/App.js
+++ cp01/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import lazy from './assets/img/lazy.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div>
+        <div className="corpo">
+          <a href="#">Medicina</a>
+          <h1>O que fazer em caso de suspeita de <br /> coronavírus?</h1>
+          <p>Os sintomas mais comuns são similares aos de uma gripe. O que define um caso suspeito e <br /> quando ir para o hospital ou fazer exame?</p>
+          <span>Por nome do aluno atualizado em 18 de ago 2020, 10h47 - Publicado em 30 mar 2020, 17h14</span>
+        </div>
+        <img src={lazy}/>
+        <div>
+          <figcaption>Se houver suspeita de coronavírus, a orientação é se isolar. O hospital deve ser procurado se os sintomas se agravarem. ilustração Daniel Almeida/SAÚDE é Vital</figcaption>
+        </div>
+      </div>
     </div>
   );
 }
